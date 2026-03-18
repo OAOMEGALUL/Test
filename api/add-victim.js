fa216f4169d3f4e3c5d638818d40d4460d38fbf5
wav3lon <149956826+Wav3lonNights@users.noreply.github.com>
2026-03-18 22:22:05 +0500

    more realistic pls

--- api/add-victim.js
+++ api/add-victim.js
@@ -8,7 +8,7 @@ const supabase = createClient(
 module.exports = async (req, res) => {
   // Разрешаем только POST запросы (отправка формы)
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: '404 No found' });
+    return res.status(405).json({ error: '404 Not Found' });
   }
 
   const { name, description, link, photo_url, secret_key } = req.body;
